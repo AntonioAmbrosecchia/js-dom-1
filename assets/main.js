87a09d30bd8a06b3cec905e2187ff1f31345584f
“AntonioAmbrosecchia” <antonyambro96@gmail.com>
2025-02-18 17:04:50 +0100

    work in progress

--- assets/main.js
+++ assets/main.js
@@ -1,2 +1,11 @@
-const prova = ("Prova");
-console.log(prova);
+document.getElementById("bottone").addEventListener("click", () => {
+    let img = document.getElementById("immagine");
+
+    if (img.src.includes("./assets/img/white_lamp.png")) {
+        img.src = "./assets/img/yellow_lamp.png";
+    } else {
+        img.src = "./assets/img/white_lamp.png";
+    }
+});
+
+
